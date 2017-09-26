881f8eb1edac7145e975996219b18d498846f7be
jdurago <jdurago@gmail.com>
2017-09-26 03:48:28 +0000

    MVP of Lorenz Curve

--- histogram.py
+++ histogram.py
@@ -17,38 +17,41 @@ data2 = np.random.uniform(0,1,10000)
 print 'Gini of Data1: ', gini(data1)
 print 'Gini of Data2: ', gini(data2)
 
+trace0 = go.Scatter(
+                    x=[0,1],
+                    y=[0,1],
+                    name='Line of Ideal Centralization',
+                    mode='lines'
+
+                )
 trace1 = go.Histogram(
                     x=data1,
                     histnorm='probability density',
-                    name='power distribution',
+                    name='Lorenz Curve',
                     cumulative=dict(enabled=True)
 
                 )
 
-trace2 = go.Histogram(
-                    x=data2,
-                    histnorm='probability density',
-                    name='unform',
-                    cumulative=dict(enabled=True)
+data = [trace0, trace1]
 
-                )
-
-data = [trace1, trace2]
+layout = go.Layout(
+                title='ETH Mining Decentralization (by Block Reward)',
+                showlegend=True,
+                legend=go.Legend(
+                    x=0,
+                    y=1.0
+                ),
+                xaxis=dict(title='Cumulative Percentage of Eth Miners'),
+                yaxis=dict(title='Cumulative Percentage of Block Rewards'),
+                margin=go.Margin(l=40, r=0, t=40, b=30)
+            )
 
 app.layout = html.Div(children=[
     dcc.Graph(
         id='my-graph',
         figure=go.Figure(
             data=data,
-            layout=go.Layout(
-                title='Lorenz Curve of ETH',
-                showlegend=True,
-                legend=go.Legend(
-                    x=0,
-                    y=1.0
-                ),
-                margin=go.Margin(l=40, r=0, t=40, b=30)
-            )
+            layout= layout
         ),
 
     )
